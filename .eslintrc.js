25ed546bf231e0a65cb393a0ffdc5a3d9b6dfd81
Namju <namuju_hi@naver.com>
2024-04-01 03:19:54 +0900

    build: storybook 기본 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@oechul/eslint-config/library.js"],
+  extends: ["@oechul/eslint-config/library.js", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
