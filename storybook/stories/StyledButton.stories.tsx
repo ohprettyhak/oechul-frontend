3d35753c4660a9354a177965b03be0b6f3fdc928
Namju <namuju_hi@naver.com>
2024-04-09 14:07:00 +0900

    chore: tsconfig.json 수정

--- storybook/stories/StyledButton.stories.tsx
+++ storybook/stories/StyledButton.stories.tsx
@@ -1,18 +1,17 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { StoryFn, Meta } from '@storybook/react';
 
 import StyledButton from './StyledButton';
 
-// 이 부분은 메타데이터 정의이며 이미 올바르게 설정하셨습니다.
 export default {
   title: 'stories/Button',
   component: StyledButton,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof StyledButton>;
+} as Meta<typeof StyledButton>;
 
 // 템플릿 정의
-const Template: ComponentStory<typeof StyledButton> = args => (
+const Template: StoryFn<typeof StyledButton> = args => (
   <StyledButton {...args} />
 );
 
@@ -23,7 +22,6 @@ Primary.args = {
   backgroundColor: '#424242',
 };
 
-// 다른 변형을 추가할 수도 있습니다.
 export const Secondary = Template.bind({});
 Secondary.args = {
   ...Primary.args,
