e1338470e9698bec64f1ac51ab89d19bf05423f0
ohprettyhak <haklee.googl@gmail.com>
2024-05-03 07:52:51 +0000

    ci: ui type module 변경

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  ignorePatterns: ['apps/**', 'packages/**'],
-  extends: ['@oechul/eslint-config/library.js'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-};
