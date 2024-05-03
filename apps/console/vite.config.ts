590f29a30662d5a740582edca61905281af723dd
Hak Lee <haklee.googl@gmail.com>
2024-05-03 16:08:04 +0900

    [CI] Vercel 호스팅 연결 (#32)

--- apps/console/vite.config.ts
+++ apps/console/vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
-    outDir: './build',
+    outDir: './dist',
   },
 });
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../
+mkdir output
+cp -R ./oechul-frontend/* ./output
+cp -R ./output ./oechul-frontend/
