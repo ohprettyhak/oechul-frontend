961bf8caee1f1ed4eeaa23e830a4e7408bace960
ohprettyhak <haklee.googl@gmail.com>
2024-05-03 16:29:39 +0900

    ci: vercel proxy 설정

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,22 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    svgr({
-      svgrOptions: {
-        exportType: 'named',
-        ref: true,
-        svgo: false,
-        titleProp: true,
-      },
-      include: '**/*.svg',
-    }),
-  ],
+  plugins: [react(), tsconfigPaths()],
   build: {
     outDir: './dist',
   },
